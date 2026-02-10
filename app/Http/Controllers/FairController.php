cfcc7c9858ad346ef3b4d26fb7594204304900e4
jorgedevelopers <joquiroga@institutosanmartin.edu.ar>
2026-02-10 13:42:32 +0000

    Fix fairs index by providing ferias from controller

--- app/Http/Controllers/FairController.php
+++ app/Http/Controllers/FairController.php
@@ -8,23 +8,69 @@ class FairController extends Controller
 {
     public function index()
     {
-        $ferias = config('ferias');
+        $ferias = [
+            [
+                'id' => 1,
+                'nombre' => 'Feria Barrial Centro',
+                'ciudad' => 'Rosario',
+                'tipo' => 'Barrial',
+                'badge' => 'success',
+                'descripcion' => 'Una feria barrial con emprendedores locales.'
+            ],
+            [
+                'id' => 2,
+                'nombre' => 'Feria Artesanal Norte',
+                'ciudad' => 'Córdoba',
+                'tipo' => 'Artesanal',
+                'badge' => 'warning',
+                'descripcion' => 'Feria de productos artesanales y diseño independiente.'
+            ],
+            [
+                'id' => 3,
+                'nombre' => 'Feria Cultural Sur',
+                'ciudad' => 'Buenos Aires',
+                'tipo' => 'Cultural',
+                'badge' => 'primary',
+                'descripcion' => 'Espacio cultural con música, arte y gastronomía.'
+            ],
+        ];
 
-        return view('fairs.index', [
-            'ferias' => $ferias,
-        ]);
+        return view('fairs.index', compact('ferias'));
     }
 
     public function show($id)
     {
-        $ferias = config('ferias');
+        $ferias = [
+            1 => [
+                'id' => 1,
+                'nombre' => 'Feria Barrial Centro',
+                'ciudad' => 'Rosario',
+                'tipo' => 'Barrial',
+                'badge' => 'success',
+                'descripcion' => 'Una feria barrial con emprendedores locales.'
+            ],
+            2 => [
+                'id' => 2,
+                'nombre' => 'Feria Artesanal Norte',
+                'ciudad' => 'Córdoba',
+                'tipo' => 'Artesanal',
+                'badge' => 'warning',
+                'descripcion' => 'Feria de productos artesanales y diseño independiente.'
+            ],
+            3 => [
+                'id' => 3,
+                'nombre' => 'Feria Cultural Sur',
+                'ciudad' => 'Buenos Aires',
+                'tipo' => 'Cultural',
+                'badge' => 'primary',
+                'descripcion' => 'Espacio cultural con música, arte y gastronomía.'
+            ],
+        ];
 
-        if (!isset($ferias[$id])) {
-            abort(404);
-        }
+        abort_unless(isset($ferias[$id]), 404);
 
         return view('fairs.show', [
-            'feria' => $ferias[$id],
+            'feria' => $ferias[$id]
         ]);
     }
 }
