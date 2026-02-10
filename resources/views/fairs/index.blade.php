b35186831aad928294bb33dfd7f3a55759984537
jorgedevelopers <joquiroga@institutosanmartin.edu.ar>
2026-02-10 13:36:17 +0000

    Make fairs index dynamic using controller data

--- resources/views/fairs/index.blade.php
+++ resources/views/fairs/index.blade.php
@@ -5,9 +5,7 @@
 @section('content')
 
 <section class="mb-4">
-    <h1 class="fw-bold">
-        Todas las ferias
-    </h1>
+    <h1 class="fw-bold">Todas las ferias</h1>
     <p class="text-muted">
         Explorá ferias barriales, artesanales y culturales de todo el país.
     </p>
@@ -15,74 +13,31 @@
 
 <div class="row g-4">
 
-    {{-- Feria 1 --}}
-    <div class="col-md-4">
-        <div class="card h-100 shadow-sm border-0 card-hover">
-            <div class="card-body d-flex flex-column">
-                <span class="badge bg-success mb-2 align-self-start">
-                    Barrial
-                </span>
+    @foreach ($ferias as $feria)
+        <div class="col-md-4">
+            <div class="card h-100 shadow-sm border-0 card-hover">
+                <div class="card-body d-flex flex-column">
 
-                <h5 class="card-title">
-                    Feria Barrial Centro
-                </h5>
+                    <span class="badge bg-{{ $feria['badge'] }} mb-2 align-self-start">
+                        {{ $feria['tipo'] }}
+                    </span>
 
-                <p class="text-muted mb-4">
-                    Rosario
-                </p>
+                    <h5 class="card-title">
+                        {{ $feria['nombre'] }}
+                    </h5>
 
-                <a href="/ferias/1" class="btn btn-outline-success mt-auto">
-                    Ver detalle
-                </a>
+                    <p class="text-muted mb-4">
+                        {{ $feria['ciudad'] }}
+                    </p>
+
+                    <a href="/ferias/{{ $feria['id'] }}" class="btn btn-outline-success mt-auto">
+                        Ver detalle
+                    </a>
+
+                </div>
             </div>
         </div>
-    </div>
-
-    {{-- Feria 2 --}}
-    <div class="col-md-4">
-        <div class="card h-100 shadow-sm border-0 card-hover">
-            <div class="card-body d-flex flex-column">
-                <span class="badge bg-warning text-dark mb-2 align-self-start">
-                    Artesanal
-                </span>
-
-                <h5 class="card-title">
-                    Feria Artesanal Norte
-                </h5>
-
-                <p class="text-muted mb-4">
-                    Córdoba
-                </p>
-
-                <a href="/ferias/2" class="btn btn-outline-success mt-auto">
-                    Ver detalle
-                </a>
-            </div>
-        </div>
-    </div>
-
-    {{-- Feria 3 --}}
-    <div class="col-md-4">
-        <div class="card h-100 shadow-sm border-0 card-hover">
-            <div class="card-body d-flex flex-column">
-                <span class="badge bg-primary mb-2 align-self-start">
-                    Cultural
-                </span>
-
-                <h5 class="card-title">
-                    Feria Cultural Sur
-                </h5>
-
-                <p class="text-muted mb-4">
-                    Buenos Aires
-                </p>
-
-                <a href="/ferias/3" class="btn btn-outline-success mt-auto">
-                    Ver detalle
-                </a>
-            </div>
-        </div>
-    </div>
+    @endforeach
 
 </div>
 
