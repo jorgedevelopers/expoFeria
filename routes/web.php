435baa0caa70b53e2b528a711463e0dd78b80f1e
jorgedevelopers <joquiroga@institutosanmartin.edu.ar>
2026-02-10 11:42:27 +0000

    Make fair detail dynamic by id (fake data)

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,38 @@ Route::get('/', function () {
 Route::get('/ferias', function () {
     return view('fairs.index');
 });
+
 Route::get('/ferias/{id}', function ($id) {
+
+    $ferias = [
+        1 => [
+            'nombre' => 'Feria Barrial Centro',
+            'ciudad' => 'Rosario',
+            'tipo' => 'Barrial',
+            'descripcion' => 'Feria comunitaria con emprendedores locales.',
+            'badge' => 'success',
+        ],
+        2 => [
+            'nombre' => 'Feria Artesanal Norte',
+            'ciudad' => 'Córdoba',
+            'tipo' => 'Artesanal',
+            'descripcion' => 'Artesanos y productores regionales.',
+            'badge' => 'warning',
+        ],
+        3 => [
+            'nombre' => 'Feria Cultural Sur',
+            'ciudad' => 'Buenos Aires',
+            'tipo' => 'Cultural',
+            'descripcion' => 'Eventos culturales y música en vivo.',
+            'badge' => 'primary',
+        ],
+    ];
+
+    if (!isset($ferias[$id])) {
+        abort(404);
+    }
+
     return view('fairs.show', [
-        'id' => $id
+        'feria' => $ferias[$id],
     ]);
 });
-
